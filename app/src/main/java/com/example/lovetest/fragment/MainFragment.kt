45426915c666681cebd10f37efd84a427124534f
segun <segun408@gmail.com>
2022-10-05 11:55:37 +0900

    btn_next2/4

--- app/src/main/java/com/example/lovetest/fragment/MainFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/MainFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +24,20 @@ class MainFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+            super.onViewCreated(view, savedInstanceState)
+            navController = Navigation.findNavController(view)
+            btn_next.setOnClickListener {
+                navController.navigate(R.id.action_mainFragment_to_questionFragment)
+            }
         }
     }
 
--- app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/QuestionFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +21,11 @@ private const val ARG_PARAM2 = "param2"
  * Use the [QuestionFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class QuestionFragment : Fragment() {
+class QuestionFragment : Fragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var navController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,11 @@ class QuestionFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        btn_next.setOnClickListener(this)
     }
 
     override fun onCreateView(
@@ -56,5 +66,13 @@ class QuestionFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_next -> {
+                navController.navigate(R.id.action_questionFragment_to_selectionFragment)
+            }
+        }
     }
 }
